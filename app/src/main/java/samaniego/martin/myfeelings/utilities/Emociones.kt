6c9a7f775ca71c0ceb048ff07dc6ff8807d07820
Martinsama97 <martinsamaniego97@hotmail.com>
2023-03-09 23:26:42 -0700

    Final

---
+++ app/src/main/java/samaniego/martin/myfeelings/utilities/Emociones.kt
@@ -0,0 +1,4 @@
+package samaniego.martin.myfeelings.utilities
+
+data class Emociones(var nombre: String, var porcentaje:Float, var color: Int, var total: Float ) {
+}
---
+++ app/src/main/java/samaniego/martin/myfeelings/utilities/JSONFile.kt
@@ -0,0 +1,35 @@
+package samaniego.martin.myfeelings.utilities
+
+import android.content.Context
+import android.util.Log
+import java.io.IOError
+import java.io.IOException
+
+class JSONFile {
+
+    val MY_FEELINGS = "data.json"
+
+    constructor(){
+
+    }
+
+    fun saveData(context: Context, json: String){
+        try {
+            context.openFileOutput(MY_FEELINGS, Context.MODE_PRIVATE).use {
+                it.write((json.toByteArray()))
+            }
+        } catch (e: IOException){
+            Log.e("GUARDAR", "Error in writing: " + e.localizedMessage)
+        }
+    }
+
+    fun getData(context: Context):String {
+        try {
+            return context.openFileInput(MY_FEELINGS).bufferedReader().readLine()
+
+        } catch (e: IOException){
+            Log.e("OBTENER", "Error in fetching data: " + e.localizedMessage)
+            return ""
+        }
+    }
+}
